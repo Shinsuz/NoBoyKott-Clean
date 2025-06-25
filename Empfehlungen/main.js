b6defa10a8f0825a0fd9301159a6b5e505538753
Shinsuz <infoshinsuz@gmail.com>
2025-06-25 02:45:30 +0200

    updated webhook

--- Empfehlungen/main.js
+++ Empfehlungen/main.js
@@ -25,7 +25,7 @@ function renderList() {
 }
 
 function sendToDiscord(msg) {
-    const webhookUrl = "https://discord.com/api/webhooks/1387159940405137450/qY8oqVjukS4mXL6RPaAZSUoD9Mua_a4_RUDFKxwITjWjiWlaGd8-932_h19F4lZrEsLd"; // <-- hier deine Webhook-URL eintragen
+    const webhookUrl = "https://discord.com/api/webhooks/1387231845841633373/GaN1sxowNIE4fFyTNBxNQp8EmF8B_pdqA40Kv9WaxhDdCcIHgtkgCAymgJZNq_I0-ruq"; // <-- hier deine Webhook-URL eintragen
     fetch(webhookUrl, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
