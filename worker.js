edf04086a176e4e0547eb162b2cf222649f21520
Shinsuz <infoshinsuz@gmail.com>
2025-06-30 01:26:34 +0200

    .

--- worker.js
+++ worker.js
@@ -2,14 +2,6 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const path = url.pathname === "/" ? "/index.html" : url.pathname;
-
-    try {
-      return await env.ASSETS.fetch(new Request(path, request));
-    } catch (err) {
-      return new Response("404 – Datei nicht gefunden", {
-        status: 404,
-        headers: { "Content-Type": "text/plain" }
-      });
-    }
+    return await env.__STATIC_CONTENT.fetch(new Request(path, request));
   }
 }
